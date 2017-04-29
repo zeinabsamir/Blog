103f2a40057ce35382104f7ff5a1a55757745b89
zeinabsamir <zeinabsamir8@gmail.com>
2017-04-29 20:27:48 +0200

    Fixed edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,11 @@ class ArticlesController < ApplicationController
   def new
    @article = Article.new
   end
-
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def  update
    @article = Article.find(params[:id])
    if @article.update(article_params)
     redirect_to @article
